7052bee8fc0f8a03cc751c870f0baaa1b47226fc
dqrtec <dqrtec@hotmail.com>
2023-07-31 23:22:25 -0300

    new start

---
+++ mancalalib.Testes/ContagemPontos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform;
+using Xunit;
+
+namespace mancalalib.Testes
+{
+    public class ContagemPontos
+    {
+        Tabuleiro _tabuleiro;
+        public ContagemPontos()
+        {
+            _tabuleiro = new Tabuleiro();
+        }
+
+        [Fact]
+        public void TesteBase()
+        {
+            Assert.Equal(0, _tabuleiro.pontosFavor());
+
+        }
+    }
+}
